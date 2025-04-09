4040104a7340935d494b5d8198054879637081b0
hamzaozalp <hamzaozalp@yaani.com>
2025-04-10 00:16:00 +0300

    ....

---
+++ MyPortfolioUdemy/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolioUdemy.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string İcon { get; set; }
+    }
+}
